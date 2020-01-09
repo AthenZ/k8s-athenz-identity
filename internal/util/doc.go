2be766f9a3d2fdb2b96f83bc8682b25ed5a60500
Michael Cieplak <michael.cieplak@gmail.com>
2020-01-09 14:35:47 -0800

    Cleanup repo towards opa based approach (#8)

--- internal/util/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2017, Yahoo Holdings Inc.
-// Licensed under the terms of the BSD-3-Clause license. See LICENSE file for terms.
-
-// Package util provides shared low-level utilities.
-package util
--- k8s/README.md
+++
@@ -1,4 +0,0 @@
-k8s
----
-
-Setup the initializer/ callback on a dev k8s cluster that you own. Do not do any of this on shared clusters!
